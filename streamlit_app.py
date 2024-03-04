af4df797ed08fc809006f9c1f07bd54185fd2731
Kiran5000 <nunavathkirannayak2000@gmail.com>
2024-03-04 23:21:30 +0530

    update all the values

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,5 @@
 import streamlit as st
 from lyzr import VoiceBot
-import tempfile
-import shutil
-import os
-import io
-import base64
-from pydub import AudioSegment
 
 # Load the API key from secrets.toml
 api_key = st.secrets["OPENAI_API_KEY"]
@@ -42,29 +36,9 @@ def main():
         text = st.text_area("Enter Text")
         if st.button("Convert"):
             # Convert text to speech
-            audio_bytes = vb.text_to_speech(text)
-            # Convert WAV audio to MP3 format
-            audio = AudioSegment.from_wav(io.BytesIO(audio_bytes))
-            with io.BytesIO() as output_buffer:
-                audio.export(output_buffer, format="mp3")
-                mp3_audio_bytes = output_buffer.getvalue()
-            # Save audio to a temporary file
-            with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_audio_file:
-                temp_audio_file.write(mp3_audio_bytes)
-                audio_file_path = temp_audio_file.name
-            # Provide download link to the user
-            st.audio(mp3_audio_bytes, format='audio/mp3')
+            vb.text_to_speech(text)
+            st.write("Text converted to speech. Check the output audio file.")
             st.success("Text converted to speech successfully.")
-            st.markdown(get_binary_file_downloader_html(audio_file_path, 'Audio File'), unsafe_allow_html=True)
-            os.unlink(audio_file_path)  # Delete the temporary audio file after providing download link
-
-# Function to generate a download link for files
-def get_binary_file_downloader_html(bin_file, file_label='File'):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    bin_str = base64.b64encode(data).decode()
-    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">{file_label}</a>'
-    return href
 
 if __name__ == "__main__":
     main()
