ff43f1f835fb9a88c92ed3715f6bbc2580c71f80
Kiran5000 <nunavathkirannayak2000@gmail.com>
2024-03-04 12:50:50 +0530

    update the files

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,48 +1,52 @@
+import os
 import openai
 import lyzr
-import os
-
 import streamlit as st
-from lyzr import QABot
+from lyzr import ChatBot
 
-def initialize_qabot(video_url, api_key):
+# Function to initialize the ChatBot with a text file
+def initialize_chatbot(text_file_path, api_key):
     # Define parameters for the Weaviate vector store
+    # You can choose either an external Weaviate cluster or local Embedded Weaviate vector store
+    # Replace the placeholders with actual values if using an external Weaviate cluster
     vector_store_params = {
         "vector_store_type": "WeaviateVectorStore",
         "url": "https://sample..weaviate.network",
-        "api_key": api_key,
+        "api_key": "DB_API_KEY",
         "index_name": "IndexName"  # first letter should be capital
     }
     
-    # Initialize the QABot with YouTube videos
-    qa_bot = QABot.youtube_qa(urls=[video_url], vector_store_params=vector_store_params)
-    return qa_bot
+    # Initialize the ChatBot with the text file
+    chatbot = ChatBot.txt_chat(input_files=[text_file_path], vector_store_params=vector_store_params)
+    return chatbot
 
-def query_qabot(qa_bot, question):
-    response = qa_bot.query(question)
+# Function to query the ChatBot with a given question
+def query_chatbot(chatbot, question):
+    # Ask a question related to the text content
+    response = chatbot.chat(question)
     return response.response
 
 def main():
-    st.title("YouTube Question-Answering with QABot")
+    st.title("Docx ChatBot")
 
     # Retrieve the API key from Streamlit secrets
     api_key = st.secrets["OPENAI_API_KEY"]
 
-    # Get the YouTube video URL from user input
-    video_url = st.text_input("Enter the YouTube video URL:")
+    # Get the path to the text file from user input
+    text_file_path = st.text_input("Enter the path to your text file (e.g., /path/to/your/file.txt):")
     
-    # Get the question from user input
-    question = st.text_input("Enter your question:")
-    
-    # Initialize the QABot
-    if video_url:
-        qa_bot = initialize_qabot(video_url, api_key)
+    # Initialize the ChatBot
+    if text_file_path:
+        chatbot = initialize_chatbot(text_file_path, api_key)
 
-        # Query the QABot with the question
+        # Get the question from user input
+        question = st.text_input("Enter your question:")
+
+        # Query the ChatBot with the question
         if st.button("Ask"):
             if question:
-                response = query_qabot(qa_bot, question)
-                st.write("QABot's Response:")
+                response = query_chatbot(chatbot, question)
+                st.write("ChatBot's Response:")
                 st.write(response)
             else:
                 st.warning("Please enter a question.")
